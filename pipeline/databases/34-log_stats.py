924c1c2339e1d50779e483377dc6ad93291d4d6a
Geu Aguto Garang <133713642+Geu-Pro2023@users.noreply.github.com>
2025-03-09 02:55:57 +0200

    Update 34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
-""" Log stats """
+""" Log stats with corrected field names """
 from pymongo import MongoClient
 
 
 def log_stats():
     """ Provides stats about Nginx logs stored in MongoDB """
-    client = MongoClient()
+    client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
 
+    # Total logs count
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
+    # Methods count
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
+    # Status check count
     status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
     print(f"{status_check} status check")
+
+    client.close()
 
 
 if __name__ == "__main__":
